f612ce18548c67e05a3418f10fc593ebfe477bc8
liangtie.qian <yihuo0420@163.com>
2023-11-10 18:19:49 +0800

    Init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# wx-drawing-tutorial
+
+This is the wx-drawing-tutorial project.
+
+Adjusted from https://github.com/lszl84/wx_drawing_tutorial
---
+++ source/drawingcanvas.h
@@ -0,0 +1,45 @@
+#ifndef C96165B8_A1FC_4864_829F_0DBB9140A003
+#define C96165B8_A1FC_4864_829F_0DBB9140A003
+
+#include <wx/wx.h>
+#include <list>
+
+#include "graphicobject.h"
+
+wxDECLARE_EVENT(CANVAS_RECT_ADDED, wxCommandEvent);
+wxDECLARE_EVENT(CANVAS_RECT_REMOVED, wxCommandEvent);
+
+class DrawingCanvas : public wxWindow
+{
+public:
+    DrawingCanvas(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size);
+    virtual ~DrawingCanvas() {}
+
+    void addRect(int width, int height, int centerX, int centerY, double angle, wxColor color, const std::string &text);
+    void removeTopRect();
+
+    int getObjectCount() { return objectList.size(); }
+
+private:
+    void OnPaint(wxPaintEvent &evt);
+    void OnMouseDown(wxMouseEvent &event);
+    void OnMouseMove(wxMouseEvent &event);
+    void OnMouseUp(wxMouseEvent &event);
+    void OnMouseLeave(wxMouseEvent &event);
+
+    void finishDrag();
+    void finishRotation();
+
+    void sendRectAddedEvent(const wxString &rectTitle);
+    void sendRectRemovedEvent(const wxString &rectTitle);
+
+    std::list<GraphicObject> objectList;
+
+    GraphicObject *draggedObj;
+    bool shouldRotate;
+
+    wxPoint2DDouble lastDragOrigin;
+};
+
+
+#endif /* C96165B8_A1FC_4864_829F_0DBB9140A003 */
